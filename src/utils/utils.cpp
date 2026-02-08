0434e35778dd5255c95897fba88102a4c748f05d
tvidano <tvidano@gmail.com>
2026-02-08 10:30:31 -0800

    Reformat utils.

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -26,101 +26,99 @@
 #include <memory>
 #include <string>
 
-namespace chrono
-{
-  namespace vehicle
-  {
-    /// @brief Creates a steering system from a JSON file. This also supports
-    /// creating a force-based steering system.
-    /// @param filename the json filename.
-    /// @return a pointer to the created steering system.
-    std::shared_ptr<ChSteering> ReadForceSteeringJSON(const std::string &filename)
-    {
-      std::shared_ptr<ChSteering> steering;
+namespace chrono::vehicle {
 
-      rapidjson::Document d;
-      chrono::vehicle::ReadFileJSON(filename, d);
-      if (d.IsNull())
-        return nullptr;
+  std::shared_ptr<ChSteering> ReadForceSteeringJSON(const std::string &filename) {
+    std::shared_ptr<ChSteering> steering;
 
-      // Check that the given file is a steering specification file.
-      assert(d.HasMember("Type"));
-      std::string type = d["Type"].GetString();
-      assert(type.compare("Steering") == 0);
+    rapidjson::Document d;
+    chrono::vehicle::ReadFileJSON(filename, d);
+    if (d.IsNull())
+      return nullptr;
 
-      // Extract the steering type.
-      assert(d.HasMember("Template"));
-      std::string subtype = d["Template"].GetString();
+    // Check that the given file is a steering specification file.
+    assert(d.HasMember("Type"));
+    std::string type = d["Type"].GetString();
+    assert(type.compare("Steering") == 0);
 
-      // Create the steering using the appropriate template.
-      if (subtype.compare("PitmanArm") == 0)
-      {
-        steering = chrono_types::make_shared<PitmanArm>(d);
-      }
-      else if (subtype.compare("RackPinion") == 0)
-      {
-        steering = chrono_types::make_shared<RackPinion>(d);
-      }
-      else if (subtype.compare("RackPinionForce") == 0)
-      {
-        steering = chrono_types::make_shared<RackPinionForce>(d);
-      }
-      else if (subtype.compare("RotaryArm") == 0)
-      {
-        steering = chrono_types::make_shared<RotaryArm>(d);
-      }
-      else
-      {
-        throw std::runtime_error("Steering type not supported in ReadSteeringJSON.");
-      }
+    // Extract the steering type.
+    assert(d.HasMember("Template"));
+    std::string subtype = d["Template"].GetString();
 
-      return steering;
+    // Create the steering using the appropriate template.
+    if (subtype.compare("PitmanArm") == 0) {
+      steering = chrono_types::make_shared<PitmanArm>(d);
+    } else if (subtype.compare("RackPinion") == 0) {
+      steering = chrono_types::make_shared<RackPinion>(d);
+    } else if (subtype.compare("RackPinionForce") == 0) {
+      steering = chrono_types::make_shared<RackPinionForce>(d);
+    } else if (subtype.compare("RotaryArm") == 0){
+      steering = chrono_types::make_shared<RotaryArm>(d);
+    } else {
+      throw std::runtime_error("Steering type not supported in ReadSteeringJSON.");
     }
 
-    std::shared_ptr<ChTire> ReadCustomTireJSON(const std::string& filename) {
-      std::shared_ptr<ChTire> tire;
-  
-      rapidjson::Document d;
-      ReadFileJSON(filename, d);
-      if (d.IsNull())
-          return nullptr;
-  
-      // Check that the given file is a tire specification file.
-      assert(d.HasMember("Type"));
-      std::string type = d["Type"].GetString();
-      assert(type.compare("Tire") == 0);
-  
-      // Extract the tire type.
-      assert(d.HasMember("Template"));
-      std::string subtype = d["Template"].GetString();
-  
-      // Create the tire using the appropriate template.
-      if (subtype.compare("RigidTire") == 0) {
-          tire = chrono_types::make_shared<RigidTire>(d);
-      } else if (subtype.compare("TMeasyTire") == 0) {
-          tire = chrono_types::make_shared<TMeasyTire>(d);
-      } else if (subtype.compare("TMsimpleTire") == 0) {
-          tire = chrono_types::make_shared<TMsimpleTire>(d);
-      } else if (subtype.compare("FialaTire") == 0) {
-          tire = chrono_types::make_shared<FialaTire>(d);
-      } else if (subtype.compare("Pac89Tire") == 0) {
-          tire = chrono_types::make_shared<Pac89Tire>(d);
-      } else if (subtype.compare("Pac02Tire") == 0) {
-          tire = chrono_types::make_shared<Pac02Tire>(d);
-      } else if (subtype.compare("ANCFTire") == 0) {
-          tire = chrono_types::make_shared<ANCFTire>(d);
-      } else if (subtype.compare("ReissnerTire") == 0) {
-          tire = chrono_types::make_shared<ReissnerTire>(d);
-      } else if (subtype.compare("FEATire") == 0) {
-          tire = chrono_types::make_shared<FEATire>(d);
-      } else if (subtype.compare("SalaaniTire") == 0) {
-          tire = chrono_types::make_shared<SalaaniTire>(d);
-      } else {
-          throw std::invalid_argument("Tire type not supported in ReadTireJSON.");
-      }
-  
-      return tire;
+    return steering;
   }
 
-  } // namespace vehicle
-} // namespace chrono
+  std::shared_ptr<ChTire> ReadCustomTireJSON(const std::string& filename) {
+    std::shared_ptr<ChTire> tire;
+
+    rapidjson::Document d;
+    std::cout << "Reading JSON: '" << filename << "'\n";
+    ReadFileJSON(filename, d);
+    if (d.IsNull())
+        return nullptr;
+
+    // Check that the given file is a tire specification file.
+    assert(d.HasMember("Type"));
+    std::string type = d["Type"].GetString();
+    assert(type.compare("Tire") == 0);
+
+    // Extract the tire type.
+    assert(d.HasMember("Template"));
+    std::string subtype = d["Template"].GetString();
+  
+    // Create the tire using the appropriate template.
+    if (subtype.compare("RigidTire") == 0) {
+        tire = chrono_types::make_shared<RigidTire>(d);
+    } else if (subtype.compare("TMeasyTire") == 0) {
+        tire = chrono_types::make_shared<TMeasyTire>(d);
+    } else if (subtype.compare("TMsimpleTire") == 0) {
+        tire = chrono_types::make_shared<TMsimpleTire>(d);
+    } else if (subtype.compare("FialaTire") == 0) {
+        tire = chrono_types::make_shared<FialaTire>(d);
+    } else if (subtype.compare("Pac89Tire") == 0) {
+        tire = chrono_types::make_shared<Pac89Tire>(d);
+    } else if (subtype.compare("Pac02Tire") == 0) {
+        tire = chrono_types::make_shared<Pac02Tire>(d);
+    } else if (subtype.compare("ANCFTire") == 0) {
+        tire = chrono_types::make_shared<ANCFTire>(d);
+    } else if (subtype.compare("ReissnerTire") == 0) {
+        tire = chrono_types::make_shared<ReissnerTire>(d);
+    } else if (subtype.compare("FEATire") == 0) {
+        tire = chrono_types::make_shared<FEATire>(d);
+    } else if (subtype.compare("SalaaniTire") == 0) {
+        tire = chrono_types::make_shared<SalaaniTire>(d);
+    } else {
+        throw std::invalid_argument("Tire type not supported in ReadTireJSON.");
+    }
+
+    return tire;
+  }
+
+  
+  void CheckHasMember(const rapidjson::Document& d, const std::string& member_name) {
+    if (!d.HasMember(member_name.c_str())) {
+      throw std::runtime_error("Missing required member '" + member_name + "' in JSON file");
+    }
+  }
+
+  void CheckHasMember(const rapidjson::Value::ConstObject& d, const std::string& member_name) {
+    if (!d.HasMember(member_name.c_str())) {
+      throw std::runtime_error("Missing required member '" + member_name + "' in JSON file");
+    }
+  }
+
+
+} // namespace chrono::vehicle
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -8,10 +8,17 @@
 
 namespace chrono::vehicle {
 
+  /// @brief Creates a steering system from a JSON file. This also supports
+  /// creating a force-based steering system.
+  /// @param filename the json filename.
+  /// @return a pointer to the created steering system.
   std::shared_ptr<ChSteering> ReadForceSteeringJSON(const std::string &filename);
 
   std::shared_ptr<ChTire> ReadCustomTireJSON(const std::string& filename);
 
-}
+  void CheckHasMember(const rapidjson::Document& d, const std::string& member_name);
+  void CheckHasMember(const rapidjson::Value::ConstObject& d, const std::string& member_name);
+
+} // namespace chrono::vehicle
 
 #endif
